4f5366d4138e3b5edc766669da9ba93efa9a5a42
IT24100907 <it24100907@my.sliit.lk>
2026-09-14 22:17:47 +0530

    Mistake

--- src/main/java/com/cinema/movie_reservation_system/repository/CinemaManagerRepository.java
+++
@@ -1,8 +0,0 @@
-//change if needed
-package com.cinema.movie_reservation_system.repository;
-
-import com.cinema.movie_reservation_system.model.CinemaManager;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CinemaManagerRepository extends JpaRepository<CinemaManager, Long> {
-}
