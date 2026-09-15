5cb1bcfd8c1bba253fd7d094382eedbe126d7db5
IT25101311 <it25101311@my.sliit.lk>
2026-09-16 02:45:30 +0530

    feat: add MovieStatus enum

---
+++ src/main/java/com/cinema/movie_reservation_system/model/MovieStatus.java
@@ -0,0 +1,10 @@
+package com.cinema.movie_reservation_system.model;
+
+/**
+ * Lifecycle status of a movie in the catalog.
+ */
+public enum MovieStatus {
+    COMING_SOON,
+    NOW_SHOWING,
+    ENDED
+}
